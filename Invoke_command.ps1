d9c2a7645375a3c166a75e8254dc2c70bdd569e7
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 14:50:24 -0700

    invoke commands

--- Invoke_command.ps1
+++ Invoke_command.ps1
@@ -1,4 +1,5 @@
-﻿$Credential = Get-Credential
+﻿$SrvPassword = ConvertTo-SecureString "$($ENV:Password)" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ("$ENV:UserName", $SrvPassword)
 Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Date}
 
 
