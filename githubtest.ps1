c00f06db094c3ee89e5ce607b8d8d3c7d7087f3d
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 11:18:56 -0700

    changed parameter

--- githubtest.ps1
+++ githubtest.ps1
@@ -1,1 +1,2 @@
-﻿Write-Host “Followed by hello world or whatever you’d like the script to say”
+﻿add param($variabletest) 
+Write-Host “Followed by hello world or whatever you’d like the script to say add $variabletest”
