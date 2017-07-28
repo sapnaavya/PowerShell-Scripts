c512d503e89081d07de83661118b1d8886f1f5e3
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 12:57:43 -0700

    Invoke Command Test

---
+++ Invoke_command.ps1
@@ -0,0 +1,6 @@
+﻿#Login-AzureRmAccount
+$Credential = Get-Credential
+Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Content -Path C:\Users\Public\Documents\date.txt|Out-File anothercomp.txt }
+#$session = New-PSSession -ComputerName 52.175.247.14 -Credential $Credential
+#Enter-PSSession -Session $session
+#Remove-PSSession $session 
