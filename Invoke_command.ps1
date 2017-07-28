7c06c5238a5f1337e4a48c8fd35427669d7b1577
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 13:03:52 -0700

    changed Invoke command

--- Invoke_command.ps1
+++ Invoke_command.ps1
@@ -1,6 +1,6 @@
 ﻿#Login-AzureRmAccount
 $Credential = Get-Credential
-Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Content -Path C:\Users\Public\Documents\date.txt|Out-File anothercomp.txt }
+Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Content -Path C:\Users\Public\Documents\date.txt|Out-File anothercomp123.txt }
 #$session = New-PSSession -ComputerName 52.175.247.14 -Credential $Credential
 #Enter-PSSession -Session $session
 #Remove-PSSession $session 
