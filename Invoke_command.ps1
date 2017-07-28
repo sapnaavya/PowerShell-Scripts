b5a775ca90af66a710f799a0f3bfa65f71311e6a
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 14:18:23 -0700

    Updated Invoke Command

--- Invoke_command.ps1
+++ Invoke_command.ps1
@@ -3,7 +3,7 @@
 ################################################
 $SrvPassword = ConvertTo-SecureString "$($ENV:Password)" -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ("$ENV:UserName", $SrvPassword)
-Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Content -Path C:\Users\Public\Documents\date.txt|Out-File anothercomp123.txt }
+Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Date}
 ###############################################
 #$session = New-PSSession -ComputerName 52.175.247.14 -Credential $Credential
 #Enter-PSSession -Session $session
