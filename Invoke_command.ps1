db79fc6497f4c6dce03e36f22b85a61b810752b6
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 14:13:32 -0700

    Updated Invoke Commands wiht env

--- Invoke_command.ps1
+++ Invoke_command.ps1
@@ -1,6 +1,13 @@
-﻿#Login-AzureRmAccount
-$Credential = Get-Credential
+﻿##Login-AzureRmAccount
+##$Credential = Get-Credential
+################################################
+$SrvPassword = ConvertTo-SecureString "$($ENV:Password)" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ("$ENV:UserName", $SrvPassword)
 Invoke-Command -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Content -Path C:\Users\Public\Documents\date.txt|Out-File anothercomp123.txt }
+###############################################
 #$session = New-PSSession -ComputerName 52.175.247.14 -Credential $Credential
 #Enter-PSSession -Session $session
 #Remove-PSSession $session 
+
+
+
