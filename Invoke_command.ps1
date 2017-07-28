5d5be6076960a43de35207cfc642c5d826900731
sapnaavya <sapnagterawe@outlook.com>
2017-07-28 15:24:56 -0700

    changed

--- Invoke_command.ps1
+++ Invoke_command.ps1
@@ -1,4 +1,6 @@
-﻿$SrvPassword = ConvertTo-SecureString "Password#123" -AsPlainText -Force
+﻿whoami
+
+$SrvPassword = ConvertTo-SecureString "Password#123" -AsPlainText -Force
 $Credential = New-Object System.Management.Automation.PSCredential ("testadmin",$SrvPassword)
 Invoke-Command -Authentication Default -ComputerName 52.175.247.14 -Credential $Credential -ScriptBlock {Get-Date}
 
