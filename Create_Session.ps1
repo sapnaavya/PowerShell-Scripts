917ed6365ec2f29d5f1f73680043c51e200d9563
sapnaavya <sapnagterawe@outlook.com>
2017-08-01 10:37:24 -0700

    creating Session

---
+++ Create_Session.ps1
@@ -0,0 +1,6 @@
+﻿whoami
+$SrvPassword = ConvertTo-SecureString "Password#123" -AsPlainText -Force
+$Credential = New-Object System.Management.Automation.PSCredential ("testadmin",$SrvPassword)
+$session = New-PSSession -ComputerName 52.183.60.0 -Credential $Credential
+Enter-PSSession -Session $session
+(Get-ChildItem).Name
